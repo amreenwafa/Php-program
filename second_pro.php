7b5e486b0af536a4e54528f73602d5091d1cc3a2
Amreen <wafaamreen6@gmail.com>
2023-03-29 13:07:38 +0530

    updated

--- second_pro.php
+++ second_pro.php
@@ -6,7 +6,7 @@ $sum=0; $rem=0;          //initially sum and rem is 0
 for ($i =0; $i<=strlen($num);$i++)   //strlen($num)" is a function that returns the length of the string in the variable $num.
                                      // For example, if $num contains the string "23654", strlen($num) would return 5 
  {  
-    $rem=$num%10;  
+    $rem=$num%10;          //1459%10=9    it gives remainder 
     $sum = $sum + $rem; 
     $num=$num/10;  
 }  
