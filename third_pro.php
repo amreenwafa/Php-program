b29b05ca9f751b871b0f489f56702d14a628c55f
Amreen <wafaamreen6@gmail.com>
2023-03-30 18:42:58 +0530

    updated

--- third_pro.php
+++ third_pro.php
@@ -1,7 +1,7 @@
 //write a program in php to find even and odd
 <?php  
 $number=1233;  
-if($number%2==0) 
+if($number%2==0)        // % gives remainder if remainder is equal to 0 even otherwise odd 
 {  
     echo "$number is Even Number";   
    }  
