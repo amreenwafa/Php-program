5643a558f13329978c27f41e41c6e807e89100da
Amreen <wafaamreen6@gmail.com>
2023-03-31 20:14:35 +0530

    updated

--- fourth_pro.php
+++ fourth_pro.php
@@ -3,7 +3,7 @@ $count = 0;  //variable count initialize with 0
 $num = 2;    
 while ($count < 15 )  
 {  
-$div_count=0;  
+$div_count=0;  // initially divide count is 0
 for ( $i=1; $i<=$num; $i++)  
 {  
 if (($num%$i)==0)  
