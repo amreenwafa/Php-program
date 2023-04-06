260c0ff0f83648e6b0394494568e74657fbcb69d
Amreen <wafaamreen6@gmail.com>
2023-04-06 18:49:02 +0530

    updated

--- forms.php
+++ forms.php
@@ -4,9 +4,9 @@
     </head>
     <body>
         <form>
-            Name: <input type "text" name= "fname">
+            Name: <input type="text" name= "fname">
             <br> <br>
-            Age : <input type "text" name="age">
+            Age : <input type="text" name="age">
             <br> <br>
             <input type="submit" value="Submit">
        </form>
---
+++ get_form.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>form using get</title>
+    </head>
+    <body>
+        <form action="formtext.php" method "GET">
+
+            Name: <input type "text" name="name">
+               <br> <br>
+            Age: <input type "text" name="age">
+                <br> <br>
+            <input type="submit" value="Submit">       
+        </form>
+    </body>
+</html>
