1ecccdae8713abd734d5ca12e9763f97fc86c8ef
Amreen <wafaamreen6@gmail.com>
2023-04-06 17:52:45 +0530

    updated

--- forms.php
+++ forms.php
@@ -3,6 +3,11 @@
         <title>Form page</title>
     </head>
     <body>
-        
+        <form>
+            Name: <input type "text" name "fname">
+            <br> <br>
+            Age : <input type "text" name "age">
+            <br> <br>
+            <input type "submit" name "save">
     </body>
 </html>
